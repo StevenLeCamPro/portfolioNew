5c49f5be478c89334486ff21543ecf04028d0981
StevenLeCamPro <steven.lecam@outlook.fr>
2025-02-15 22:34:06 +0100

    premier ajout compétences

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import Card from "@/components/Card";
+import { link } from "fs";
+
 export default function Home() {
+  const images = [
+    { src: "/PDP.jpeg", name: "Steven Le Cam" , link: "https://www.linkedin.com/in/steven-le-cam/"},
+    { src: "/PharmInnovMockup.png", name: "PharmInnov" , link: "https://www.wikipedia.fr"},
+    { src: "/vercel.svg", name: "Vercel", link: "https://www.vercel.com"},
+    { src: "/window.svg", name: "Window" , link: "https://www.microsoft.com"},
+    { src: "/ComptoirMockup.png", name: "Comptoir de la Poste",  link: "https://www.comptoirdelaposte.fr"},
+    { src: "/DAGMockup.png", name: "Doc and Go" , link: "https://www.docandgo.fr"},
+  ];
+
   return (
     <section className="flex flex-col items-center text-center py-10 relative">
+      {/* Avatar */}
       <div className="relative w-72 h-72 flex items-center justify-center">
-        <div className="absolute w-full h-full rounded-full bg-gradient-to-r from-blue-300 to-blue-500 animate-waves"></div>
+        <div className="absolute w-full h-full rounded-full bg-gradient-to-r from-blue-300 to-blue-500"></div>
         <img
           src="/PDP.jpeg"
           alt="Photo de profil"
@@ -21,22 +34,20 @@ export default function Home() {
         full-stack. Passionné par le développement web, j'ai réalisé quelques
         travaux que j'aimerais vous montrer et vous expliquer. Mon portfolio met
         en avant mes projets, qui illustrent mon travail dans la création de
-        sites web et d'applications. Il met en évidence ma créativité et mes
-        compétences dans le domaine du développement.
+        sites web et d'applications.
       </p>
 
+      {/* Bouton de téléchargement du CV */}
       <a
-        href="/LeCamStevenCV.pdf" // Assure-toi que le fichier est bien dans le dossier public
+        href="/LeCamStevenCV.pdf"
         className="relative bg-blue-300 px-6 py-3 my-4 text-lg font-semibold text-white rounded-lg overflow-hidden group transition-all duration-500"
         download
       >
-        {/* Effet de hover en arrière-plan qui démarre de la gauche */}
         <span className="absolute inset-0 scale-x-0 group-hover:scale-x-100 origin-left bg-gradient-to-r from-blue-500 to-blue-300 transition-transform duration-500 ease-out"></span>
-
-        {/* Texte au premier plan */}
         <span className="relative z-10">Téléchargez mon CV</span>
       </a>
 
+      {/* Flèche vers la section projets */}
       <a href="#projet">
         <svg
           className="w-6 h-6 mt-2 text-blue-600 group-hover:text-white animate-bounce"
@@ -54,7 +65,7 @@ export default function Home() {
         </svg>
       </a>
 
-      {/* Style de l'animation des vagues */}
+      {/* Animation des vagues */}
       <style jsx>{`
         @keyframes waveAnimation {
           0% {
@@ -70,12 +81,12 @@ export default function Home() {
             opacity: 0.7;
           }
         }
-
         .animate-waves {
           animation: waveAnimation 5s infinite ease-in-out;
         }
       `}</style>
 
+      {/* Section projets */}
       <h1 className="text-blue-400 text-4xl font-bold mt-6" id="projet">
         Mes projets
       </h1>
@@ -83,64 +94,55 @@ export default function Home() {
         Voici un aperçu de mes projets
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-6 place-items-center">
-        {/* Première carte */}
+        {/* Cartes projets */}
         <div className="relative group w-[320px] h-[220px] rounded-xl overflow-hidden">
-          {/* Image du projet */}
           <img
             src="/ComptoirMockup.png"
             alt="Comptoir de la Poste"
             className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
           />
-
-          {/* Overlay sombre + Texte caché par défaut */}
-          <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-            <p className="text-white text-lg font-bold">Comptoir de la Poste</p>
-            <p className="text-white text-sm mt-2">
-              Projet de stage de deuxième année
-            </p>
-          </div>
         </div>
 
-        {/* Deuxième carte */}
         <div className="relative group w-[320px] h-[220px] rounded-xl overflow-hidden">
-          {/* Image du projet */}
           <img
             src="/DAGMockup.png"
             alt="Doc and Go"
             className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
           />
-
-          {/* Overlay sombre + Texte caché par défaut */}
-          <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-            <p className="text-white text-lg font-bold">DOC AND GO</p>
-            <p className="text-white text-sm mt-2">
-              Projet de stage de deuxième année
-            </p>
-          </div>
         </div>
 
-        {/* Troisième carte */}
-        <div className="relative group w-[320px] h-[220px] rounded-xl overflow-hidden ">
-          {/* Image du projet avec taille réduite */}
+        <div className="relative group w-[320px] h-[220px] rounded-xl overflow-hidden">
           <img
             src="/PharmInnovMockup.png"
             alt="PharmInnov"
-            className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105"
+            className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
           />
-
-          {/* Overlay sombre + Texte caché par défaut */}
-          <div className="absolute inset-0 bg-black bg-opacity-70 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-            <p className="text-white text-lg font-bold">
-              SAE 5.01 : PharmInnov
-            </p>
-            <p className="text-white text-sm mt-2">Projet de cours</p>
-          </div>
         </div>
       </div>
+
+      {/* Bouton Voir plus de projets */}
       <a className="relative bg-blue-300 px-6 py-3 my-4 text-lg font-semibold text-white rounded-lg overflow-hidden group transition-all duration-500">
         <span className="absolute inset-0 scale-x-0 group-hover:scale-x-100 origin-right bg-gradient-to-r from-blue-300 to-blue-500 transition-transform duration-500 ease-out"></span>
         <span className="relative z-20 block">Voir plus de projets</span>
       </a>
+
+        <div className="flex flex-col items-center text-center py-10 relative">
+      <h1 className="text-blue-400 text-4xl font-bold mt-6" id="projet">
+        Mes Compétences
+      </h1>
+      {/* Carrousel infini sans animation */}
+      <div className="flex gap-4 mt-10">
+      
+        {images.map((image, index) => (
+          <>
+          <div className="flex flex-col items-center text-center">
+          <Card image={image.src} key={index} link={image.link} />
+          <p className="text-lg text-gray-600 mt-4 max-w-2xl">{image.name}</p>
+          </div>
+          </>
+        ))}
+      </div>
+      </div>
     </section>
   );
 }
---
+++ src/components/Card.tsx
@@ -0,0 +1,51 @@
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useState } from "react";
+
+
+import Image from "next/image";
+
+interface CardProps {
+  image: string;
+  link:string;
+}
+
+const Card: React.FC<CardProps> = ({ image, link }) => {
+  const [showOverlay, setShowOverlay] = useState(false);
+
+  return (
+    <motion.div
+      className="relative overflow-hidden h-[200px] min-w-[200px] rounded-xl flex justify-center items-center"
+      key={image}
+      onHoverStart={() => setShowOverlay(true)}
+      onHoverEnd={() => setShowOverlay(false)}
+    >
+      {/* Hover overlay */}
+      <AnimatePresence>
+        {showOverlay && (
+          <motion.div
+            className="z-10 flex justify-center items-center"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <div className="absolute bg-black pointer-events-none opacity-50 h-full w-full" />
+            <motion.h1
+              className="bg-white font-semibold text-sm z-10 px-3 py-2 rounded-full flex items-center gap-[0.5ch] hover:opacity-75"
+              initial={{ y: 10 }}
+              animate={{ y: 0 }}
+              exit={{ y: 10 }}
+            >
+                <a href={link}>
+                <span className="text-black">Explore Now</span>
+              </a>
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M19.071 4.929a10 10 0 1 0 0 14.142 10.011 10.011 0 0 0 0-14.142zm-1.414 12.728a8 8 0 1 1 0-11.314 8.01 8.01 0 0 1 0 11.314z"/><path d="M9 10h3.586l-4.293 4.293 1.414 1.414L14 11.414V15h2V8H9v2z"/></svg>
+            </motion.h1>
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <Image src={image} alt={image} fill style={{ objectFit: "cover" }} />
+    </motion.div>
+  );
+};
+
+export default Card;
