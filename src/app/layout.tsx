791900890d10d3263a194c68d80d68eead97631e
StevenLeCamPro <steven.lecam@outlook.fr>
2025-02-17 21:24:17 +0100

    fix bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
 import type { Metadata } from "next";
+import { ReactNode } from "react";
 
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-
-
 
 export const metadata: Metadata = {
   title: "Portfolio - Steven Le Cam",
   description: "Bienvenue sur mon Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="fr">
       <body className="bg-cyan-200 flex flex-col min-h-screen">
